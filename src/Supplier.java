f62a90bf9dc146383b4c9236bade9b2d3f434036
marioQC16 <63430849+marioQC16@users.noreply.github.com>
2021-02-09 17:11:30 -0600

    Update Supplier.java

--- src/Supplier.java
+++ src/Supplier.java
@@ -25,7 +25,7 @@ public class Supplier{
 // changing the supplier's name
     public void changeSName(String newName){
 
-        thi.sName = newName;
+        this.sName = newName;
         return;
     }
 // adding an item to the product list
@@ -56,39 +56,3 @@ public class Supplier{
 
     }
 
-
-
-
-
-
-
-    private String pName;  //unique identifier
-    private int stockQty;  //negative means there are reservations on the item
-    private LinkedList<int> sList;  //list of suppliers with the product
-
-    public Product(String pName){   //the pName should be referenced 
-                                    //by the global product list before constructing to check for duplicates
-        this.pName = pName;
-        this.stockQty = 0;
-        this.sList = new LinkedList<int>();
-    }
-    public void nameItem(String newPName){
-        this.pName = newPName;
-        return;
-    }
-    public boolean inStock(){
-        if(this.stockQty > 0){
-            return true
-        }
-        return false;
-    }
-    public int getStock(){ //amount in stock
-        return this.stockQty;
-    }
-    public void addStock(int qty){
-        this.stockQty += qty;
-        return;
-    }
-    public boolean removeStock(int qty){
-        this.stockQty += (qty * -1);
-    }
