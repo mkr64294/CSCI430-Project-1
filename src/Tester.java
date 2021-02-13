5c9f8365be89dcf980c09aeb172602d3d6bc0f8f
Khim <shresthakhim12@gmail.com>
2021-02-13 11:45:01 -0600

    Shopping cart methods Tested

--- src/Tester.java
+++ src/Tester.java
@@ -5,7 +5,24 @@ import java.io.*;
 public class Tester {
 
   public static void main(String[] args) {
-  
+
+    ShoppingCart sc = new ShoppingCart();
+    
+    sc.addItem("charger", 5);
+    sc.addItem("charger", 7);
+    sc.addItem("phone", 8);
+    sc.addItem("phone", 1);
+    
+    System.out.println(sc.showQuantity("charger"));
+    System.out.println(sc.showQuantity("phone"));
+
+    System.out.println(sc.findProduct("phone").getPName() + " " + sc.findProduct("phone").getQty());
+    System.out.println(sc.findProduct("charger").getPName() + " " + sc.findProduct("charger").getQty());
+
+    if(sc.setItemQty("charger",12))
+    System.out.println("worked");
+    
+
   Client c1 = new Client(1, "khim");
   Client c2 = new Client(2, "Dhoni");
   
@@ -35,7 +52,5 @@ public class Tester {
   clientlist.insertClient(c2);
   
   System.out.println(clientlist);
- 
  }
-
 }
