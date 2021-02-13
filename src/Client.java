da1192108f12730242b274d1dca25f406aba1b24
Khim <shresthakhim12@gmail.com>
2021-02-13 14:43:45 -0600

    Updated client

--- src/Client.java
+++ src/Client.java
@@ -8,8 +8,7 @@ public class Client implements Serializable {
   private String cAddress;
   private double credit;
   private static final String MEMBER_STRING = "C";
-  private LinkedList<Product> pList = new LinkedList<>(); //list of products with quantity of item
-  private ProductList productList = new ProductList();
+ 
   public Client(int cId, String cName) {
     this.cId= cId;   
     this.cName = cName;        
@@ -55,51 +54,6 @@ public class Client implements Serializable {
 
 
 
-/*
-  //adds the product in the cart, if cart is empty. otherwise adds up the product
-    public void addCart(int supID, String pName, int qty){
-      productList.addToStock(supID, pName, qty);
-      
-      int toAddQuantityIndex = -1;
-      for (int i = 0; i < pList.size(); i++) {
-        if (pList.get(i).getPName().equalsIgnoreCase(pName)) {
-          toAddQuantityIndex = i;
-          break;
-        }
-      }
-      
-      if(toAddQuantityIndex >= 0){
-        this.pList.get(toAddQuantityIndex).addToStock(qty);
-      } else {
-        Product p = new Product(pName);
-        p.addStock(qty);
-        this.pList.add(p);      
-      } return;
-      
-    }
-
-    //remove the item from the cart or decrease the amount of prodcut 
-    public boolean removeCart(String pName, int qty){
-      int toRemoveIndex = -1;
-      for (int i = 0; i < pList.size(); i++) {
-        if (pList.get(i).getPName().equalsIgnoreCase(pName)) {
-          toRemoveIndex = i;
-          break;
-        }
-      }
-      
-      if(toRemoveIndex >= 0){
-        this.pList.remove(toRemoveIndex);
-        return true;
-      }
-      return false;
-    }
-   
-    public boolean inCart() {
-      return this.pList.size() > 0;
-    }
-
-*/
   
 
 
