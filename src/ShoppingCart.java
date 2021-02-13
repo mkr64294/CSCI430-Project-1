9f9f52f3feeac84238b10f9d71797242663bb273
Khim123 <78177232+Khim123@users.noreply.github.com>
2021-02-12 18:35:58 -0600

    Add files via upload

---
+++ src/ShoppingCart.java
@@ -0,0 +1,103 @@
+
+
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+public class ShoppingCart {
+  private int supID;
+  private LinkedList<ItemQty> items;
+ 
+  public ShoppingCart(){}; 
+
+  public ShoppingCart(int supID, LinkedList<ItemQty> items){
+    this.supID = supID;
+    this.items = items;
+  }
+
+  public int getSupID(){
+    return supID;
+  }
+
+  public void setSupID(int newID){
+    this.supID = newID;
+    return;
+  }
+	
+  public void addItem(String itemName, int qty){
+    if(findProduct(itemName).getPName() == itemName){
+       findProduct(itemName).addQty(qty);
+     } else {
+	items.add(new ItemQty(itemName, qty));
+	findProduct(itemName).setQty(qty);
+    }
+  }
+
+  public int showQuantity(String itemName){
+    if(findProduct(itemName).getPName() != "not found"){
+      return findProduct(itemName).getQty();
+    }
+    return -9999;
+  }
+
+  public ItemQty findProduct(String pName){
+    for(int i = 0; i < items.size(); i++){
+      //if(items.get(i).getPName() == pName){
+      if(items.get(i).getPName().equals(pName)){
+        return items.get(i);
+      }
+    }
+    return new ItemQty("not found", -1);
+  }
+
+  /*
+  public boolean setItemQty(String pName, int qty){
+    if(findProduct(itemName).getPName() != "not found"){
+      findProduct(itemName).setQty(qty);
+      return true;
+    }
+    return false;
+  }
+  */
+}
+
+ class ItemQty{
+  private String pName;
+  private int qty;
+  public ItemQty(String pName, int qty){
+    this.pName = pName;
+    this.qty = qty;
+  }
+
+  public String getPName(){
+    return pName;
+  }
+  public void setPName(String newName){
+    this.pName = newName;
+    return;
+  }
+  public int getQty(){
+    return this.qty;
+  }
+  public void setQty(int newQty){
+    this.qty = newQty;
+  }
+
+  public void addQty(int qty){
+    this.qty += qty;
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
